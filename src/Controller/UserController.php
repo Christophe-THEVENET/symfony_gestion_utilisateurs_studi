7f7f30b267db37de504c28264f67cf2410e089bf
Christophe THEVENET <christophethevenet2.0@gmail.com>
2022-10-07 07:31:36 +0200

    modif liste produits + liste utilisateur + test restriction ds controleur

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -18,6 +18,7 @@ class UserController extends AbstractController
     #[Route('/', name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
+        /* $this->denyAccessUnlessGranted('ROLE_ADMIN'); */  // bloque sauf les admins
         return $this->render('user/index.html.twig', [
             'users' => $userRepository->findAll(),
         ]);
